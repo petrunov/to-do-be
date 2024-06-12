b9923a96c44d54ca9342f40209d097ba19fe2561
Vesselin Petrunov <petrunov@protonmail.com>
2024-06-13 01:56:26 +0300

    Allow CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,20 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: [
+      'Content-Type',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'Authorization',
+    ],
+    exposedHeaders: ['Authorization'],
+    credentials: true,
+  });
+
   // Enable global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
