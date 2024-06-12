7d33c4b1b222b5e273dab62e51e9cfeb78e7e635
Vesselin Petrunov <petrunov@protonmail.com>
2024-06-13 02:05:09 +0300

    Allow CORS

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: [
       'Content-Type',
