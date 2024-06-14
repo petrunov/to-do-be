a25af2aa4e000c64f6de685c66b3b0c8e8ca1189
Vesselin Petrunov <petrunov@protonmail.com>
2024-06-14 18:54:27 +0300

    Refactor the code for heroku specifics

--- db/typeorm.config.ts
+++ db/typeorm.config.ts
@@ -2,6 +2,9 @@ import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 import { resolve } from 'path';
+import { parse } from 'pg-connection-string';
+
+const parsedUrl = parse(process.env.CLEARDB_DATABASE_URL);
 
 const envPath = resolve(__dirname, '../.env');
 config({ path: envPath });
@@ -10,11 +13,11 @@ const configService = new ConfigService();
 
 export default new DataSource({
   type: 'mysql',
-  host: configService.get('MYSQL_HOST'),
-  port: configService.get('MYSQL_PORT'),
-  username: configService.get('MYSQL_USER'),
-  password: configService.get('MYSQL_PASSWORD'),
-  database: configService.get('MYSQL_DB'),
+  host: parsedUrl.host,
+  port: parseInt(parsedUrl.port, 10),
+  username: parsedUrl.user,
+  password: parsedUrl.password,
+  database: parsedUrl.database,
   entities: [`${__dirname}/../src/**/*.entity{.ts,.js}`],
   synchronize: configService.get('nodenv') === 'development',
   logging: configService.get('nodenv') === 'development',
