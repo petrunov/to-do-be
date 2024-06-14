25363eda0e45abfab871e59845ab48b6a2c1c93a
Vesselin Petrunov <petrunov@protonmail.com>
2024-06-14 18:21:53 +0300

    Change the port the web server listens on Heroku

--- src/main.ts
+++ src/main.ts
@@ -40,10 +40,8 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
+  await app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
-
-  await app.listen(PORT);
 }
 bootstrap();
